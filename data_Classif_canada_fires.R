8ccafeac9491c01adf0513f9e381184d845718bc
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2024-02-23 14:50:16 -0700

    canada fires data sets

--- data_Classif_canada_fires.R
+++ data_Classif_canada_fires.R
@@ -1,31 +1,48 @@
-library(data.table)
-canada.fires <- fread(
-  "data_Classif_canada_fires/Undersampling_75.csv"
-)[, `:=`(
-  image=Id_feu,
-  y=classe3
-)]
-canada.fires[, table(Id_image, Id_feu)]
-canada.fires[, table(classe2, classe3)]
-## here is the data (Undersampling_75) that I think would work the best. Its a undersampling of my original data to have a more equal number of points by class and by image. I kept only one satellite since using images from three created to much variability in the input data.
-
-## I also put my selection of 17 best input features in a .csv file. They give the best results.
 fread("data_Classif_canada_fires/select17_vari_undersampling.csv")
-
-##     The column 'Id_feu' identifie each image/fire
-##     column 'classe3' is the real class so the output value (its a variation of 'classe2' where I combined some underrepresented classes)
-##     For the input features, you can use all the columns that start with 'mean, max, median and min', dnbr, dnbrwoff, rbr, rbrwoff, rdnbr, rdnbrwoff,NDVI, r,g,b, nir,IB, GCC, VARI
-names(canada.fires)
-keep.cols <- c(
-  "image", "y",
-  grep("mean|max|median|min", names(canada.fires), value=TRUE),
-  "dnbr", "dnbrwoff", "rbr", "rbrwoff", "rdnbr", "rdnbrwoff",
-  #"NDVI",
-  "r","g","b", "nir","IB")#, "GCC", "VARI")
-out.dt <- canada.fires[, keep.cols, with=FALSE]
-na.counts <- colSums(is.na(out.dt))
-na.counts[na.counts>0]
-##Share in public? No problem, maybe just get the column 'interprete' out
-canada.fires$interprete
-out.dt$interprete
-fwrite(out.dt, "data_Classif/CanadaFires.csv")
+library(data.table)
+fires.csv.list <- list(
+  all="All_data_SkysatImages_Gaby.csv",
+  downSampled="Undersampling_75_classe2.csv",
+  missing="Undersampling_75.csv")
+for(fires.name in names(fires.csv.list)){
+  path.csv <- file.path(
+    "data_Classif_canada_fires",
+    fires.csv.list[[fires.name]])
+  print(fires.name)
+  print(path.csv)
+  canada.fires <- fread(
+    path.csv
+  )[, `:=`(
+    image=Id_feu,
+    y=classe2
+  )]
+  print(names(canada.fires))
+  canada.fires[, table(Id_image, Id_feu)]
+  print(canada.fires[, table(classe2)])
+  ##canada.fires[, table(classe2, classe3)]
+  table(canada.fires[[5]], canada.fires[[10]])#both id_feu
+  ## here is the data (Undersampling_75) that I think would work the best. Its a undersampling of my original data to have a more equal number of points by class and by image. I kept only one satellite since using images from three created to much variability in the input data.
+## I also put my selection of 17 best input features in a .csv file. They give the best results.
+  ##     The column 'Id_feu' identifie each image/fire
+  ##     column 'classe3' is the real class so the output value (its a variation of 'classe2' where I combined some underrepresented classes)
+  ##     For the input features, you can use all the columns that start with 'mean, max, median and min', dnbr, dnbrwoff, rbr, rbrwoff, rdnbr, rdnbrwoff,NDVI, r,g,b, nir,IB, GCC, VARI
+  keep.cols <- c(
+    "image", "y",
+    grep("mean|max|median|min", names(canada.fires), value=TRUE),
+    "dnbr", "dnbrwoff", "rbr", "rbrwoff", "rdnbr", "rdnbrwoff",
+    "NDVI",
+    "r","g","b", "nir","IB", "GCC", "VARI")
+  out.dt <- canada.fires[, keep.cols, with=FALSE]
+  na.counts <- colSums(is.na(out.dt))
+  ##Share in public? No problem, maybe just get the column 'interprete' out
+  canada.fires$interprete
+  out.dt$interprete
+  if(all(na.counts==0)){
+    out.csv <- file.path(
+      "data_Classif", sprintf("CanadaFires_%s.csv", fires.name))
+    print(out.csv)
+    fwrite(out.dt, out.csv)
+  }else{
+    print(na.counts[na.counts>0])
+  }
+}
