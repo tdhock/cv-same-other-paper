6ab26e95cea698a2cc9e8f704fda0d2c25c7df9e
Toby Dylan Hocking <toby.hocking@nau.edu>
2024-03-06 17:15:19 -0700

    new data plots

--- data_Classif_batchmark_registry.R
+++ data_Classif_batchmark_registry.R
@@ -3,6 +3,7 @@ library(data.table)
 work.dir <- "/scratch/th798/cv-same-other-paper"
 reg.RData <- file.path(work.dir, "data_Classif_batchmark_registry.RData")
 (objs=load(reg.RData))
+
 meta.dt <- data.table::fread("data-meta.csv")[
   grepl("train|test",small_group), `:=`(
     test=ifelse(small_group=="test", small_N, large_N),
@@ -132,4 +133,29 @@ gg <- ggplot()+
 png("data_Classif_batchmark_registry_glmnet_mean_sd.png", width=20, height=4, units="in", res=100)
 print(gg)
 dev.off()
+
+meta.not.tt <- meta.dt[is.na(test)]
+for(meta.i in 1:nrow(meta.not.tt)){
+  meta.row <- meta.not.tt[meta.i]
+  scores.not <- score.dt[meta.row, on=.(task_id=data.name), nomatch=0L]
+  if(nrow(scores.not)){
+    gg <- ggplot()+
+      ggtitle(paste("Data set:", meta.row$data.name))+
+      geom_point(aes(
+        percent.error, train.groups, color=algorithm),
+        shape=1,
+        data=scores.not)+
+      facet_grid(. ~ test.group, labeller=label_both, scales="free")+
+      scale_x_continuous(
+        "Percent prediction error on CV test group (one dot for each of 10 folds in CV)")+
+      scale_y_discrete(
+        "Train groups")
+    out.png <- sprintf(
+      "data_Classif_batchmark_registry_glmnet_featureless_%s.png",
+      meta.row$data.name)
+    png(out.png, width=8, height=2, units="in", res=200)
+    print(gg)
+    dev.off()
+  }
+}
 system("cd /projects/genomic-ml && unpublish_data cv-same-other-paper && publish_data projects/cv-same-other-paper")
