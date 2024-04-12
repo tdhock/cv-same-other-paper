5183c807b9aa61e82ef03639537f5c459bf4a953
Toby Dylan Hocking <toby.hocking@nau.edu>
2024-04-11 22:27:16 -0700

    scatter all other p-val est

--- data_Classif_batchmark_registry.R
+++ data_Classif_batchmark_registry.R
@@ -28,9 +28,19 @@ score.dt[
 score.atomic <- score.dt[,sapply(score.dt, class)!="list", with=FALSE]
 if(FALSE){
   fwrite(score.atomic, "data_Classif_batchmark_registry.csv")
-  score.atomic <- fread("data_Classif_batchmark_registry.csv")
+  score.atomic.noerr <- fread("data_Classif_batchmark_registry_noerr.csv")
+## > setdiff(unique(score.atomic$data.name), unique(score.atomic.old$data.name))
+## [1] 
+## > setdiff(unique(score.atomic.old$data.name), unique(score.atomic$data.name))
+## [1] "QMNIST"   "spam"     "STL10"    "vowel"    "waveform" "zipUSPS"
+##EMNIST has 1 error in new, take all from noerr, plus MNIST_* data from new.
+  score.both <- rbind(
+    score.atomic[data.name%in%c("MNIST_EMNIST","MNIST_EMNIST_rot","MNIST_FashionMNIST")],
+    score.atomic.noerr)
+  (fold.counts <- dcast(score.both, data.name + train.groups + test.group ~ algorithm, length))
+  fold.counts[grepl("MNIST", data.name)]
 }
-score.join <- meta.dt[score.atomic, on="data.name"]
+score.join <- meta.dt[score.dt, on="data.name"]
 
 (tab.wide <- dcast(
   score.join[algorithm=="cv_glmnet"],
@@ -43,8 +53,18 @@ score.join <- meta.dt[score.atomic, on="data.name"]
   value.name="compare_error"
 )[, {
   test.res <- t.test(compare_error, same, paired=TRUE)
-  with(test.res, data.table(estimate, p.value))
-}, by=.(data.name,test.group,compare_name)])
+  log.signed.p <- function(signed.p){
+    sign(signed.p)*abs(log10(abs(signed.p)))
+  }
+  with(test.res, data.table(
+    estimate,
+    p.value,
+    log10.p=log10(p.value),
+    sign.log.p=log.signed.p(p.value*sign(estimate)),
+    N=.N))
+}, by=.(data.name,compare_name)])
+tab.long[order(compare_name,-estimate)]
+tab.long[order(compare_name,-sign.log.p)]
 
 (compare.wide.each <- dcast(
   tab.long,
@@ -58,17 +78,19 @@ ggplot()+
 (compare.wide <- dcast(
   tab.long,
   data.name ~ compare_name,
-  mean,
-  value.var="estimate"))
+  value.var=c("estimate","sign.log.p","log10.p")))
+(xt <- meta.dt[compare.wide, .(data.name, rows, features, classes, n.groups, "all-same"=estimate_all), on="data.name"][!grepl("MNIST_",data.name)][order(-`all-same`)])
+library(xtable)
+print(xtable(xt), type="latex")
 gg <- ggplot()+
   theme_bw()+
   geom_hline(yintercept=0,color="grey")+
   geom_vline(xintercept=0,color="grey")+
   geom_point(aes(
-    other, all, color=data.name),
+    estimate_other, estimate_all, color=data.name),
     data=compare.wide)+
   ggrepel::geom_label_repel(aes(
-    other, all, color=data.name, label=data.name),
+    estimate_other, estimate_all, color=data.name, label=data.name),
     data=compare.wide)+
   coord_equal()+
   theme(legend.position="none")+
@@ -89,7 +111,123 @@ zoom <- gg+
 png("data_Classif_batchmark_registry_scatter_other_all_similar.png", width=6, height=6, units="in", res=200)
 print(zoom)
 dev.off()
-    
+tlab <- function(x, y, label){
+  data.table(x, y, label)
+}
+text.dt <- rbind(
+  tlab(4, 0, "No significant difference"),
+  tlab(4, -11, "Highly\nsignificant\ndifference"),
+  tlab(-2, -7, "Beneficial\nto combine"),
+  tlab(8, -7, "Detrimental\nto combine"))
+gg <- ggplot(mapping=aes(
+  estimate_all, log10.p_all, color=data.name))+
+  ggtitle("Is it beneficial to combine groups?")+
+  theme_bw()+
+  ##geom_hline(yintercept=0,color="grey")+
+  geom_vline(xintercept=0,color="grey")+
+  geom_text(aes(
+    x, y, label=label, color=NULL),
+    vjust=0,
+    data=text.dt)+
+  geom_point(
+    data=compare.wide)+
+  ggrepel::geom_text_repel(aes(
+    label=data.name),
+    size=2.5,
+    data=compare.wide)+
+  theme(legend.position="none")+
+  scale_y_continuous(
+    "log10(p-value)",
+    breaks=seq(-10,0,by=2))+
+  scale_x_continuous(
+    "Percent test error difference (same-all)",
+    breaks=seq(-10,10,by=2),
+    limits=c(-3,NA))
+png("data_Classif_batchmark_registry_scatter_all.png", width=5, height=4, units="in", res=200)
+print(gg)
+dev.off()
+text.dt <- rbind(
+  ## tlab(15, 0, "No significant difference"),
+  ## tlab(15, -40, "Highly\nsignificant\ndifference"),
+  tlab(0, -30, "Same\naccuracy"),
+  tlab(70, -30, "Worse accuracy"))
+ymin <- -2
+ymax <- 0
+xmin <- -1
+xmax <- 1
+rect.dt <- data.table(xmin, xmax, ymin, ymax)
+gg <- ggplot()+
+  ggtitle("Accurate prediction on a new group?")+
+  geom_rect(aes(
+    xmin=xmin,
+    xmax=xmax,
+    ymin=ymin,
+    ymax=ymax),
+    color="grey50",
+    fill=NA,
+    data=rect.dt)+
+  theme_bw()+
+  ##geom_hline(yintercept=0,color="grey")+
+  geom_vline(xintercept=0,color="grey")+
+  geom_text(aes(
+    x, y, label=label, color=NULL),
+    vjust=0,
+    data=text.dt)+
+  geom_point(aes(
+    estimate_other, log10.p_other, color=data.name),
+    data=compare.wide)+
+  ggrepel::geom_text_repel(aes(
+    estimate_other,
+    log10.p_other,
+    color=data.name,
+    label=data.name),
+    size=2.5,
+    data=compare.wide)+
+  theme(legend.position="none")+
+  scale_y_continuous(
+    "log10(p-value)\n<- highly significant --- not significant ->",
+    limits=c(-40,0),
+    breaks=seq(-100,0,by=10))+
+  scale_x_continuous(
+    "Percent test error difference (same-other)",
+    breaks=seq(-10,100,by=10),
+    limits=c(-10,NA))
+png("data_Classif_batchmark_registry_scatter_other.png", width=7.5, height=4, units="in", res=200)
+print(gg)
+dev.off()
+text.dt <- rbind(
+  ## tlab(15, 0, "No significant difference"),
+  ## tlab(15, -40, "Highly\nsignificant\ndifference"),
+  tlab(0, -30, "Same accuracy"),
+  tlab(60, -30, "Worse accuracy"))
+gg <- ggplot(mapping=aes(
+  estimate_other, log10.p_other, color=data.name))+
+  theme_bw()+
+  ##geom_hline(yintercept=0,color="grey")+
+  geom_vline(xintercept=0,color="grey")+
+  ## geom_text(aes(
+  ##   x, y, label=label, color=NULL),
+  ##   vjust=0,
+  ##   data=text.dt)+
+  geom_point(
+    data=compare.wide)+
+  ggrepel::geom_text_repel(aes(
+    label=data.name),
+    size=3,
+    data=compare.wide)+
+  theme(legend.position="none")+
+  scale_y_continuous(
+    "log10(p-value)",
+    limits=c(ymin,ymax),
+    breaks=seq(-10,10,by=1))+
+  scale_x_continuous(
+    "Percent test error difference (same-other)",
+    breaks=seq(-10,10,by=1),
+    limits=c(xmin,xmax))
+png("data_Classif_batchmark_registry_scatter_other_zoom.png", width=4, height=3, units="in", res=200)
+print(gg)
+dev.off()
+system("cd /projects/genomic-ml && unpublish_data cv-same-other-paper && publish_data projects/cv-same-other-paper")
 
 tt.join <- score.join[
   grepl("train|test",group.small.name)
@@ -201,6 +339,7 @@ print(gg)
 dev.off()
 
 meta.not.tt <- meta.dt#[is.na(test)]
+meta.not.tt <- meta.dt[data.name=="MNIST_EMNIST_rot"]
 for(meta.i in 1:nrow(meta.not.tt)){
   cat(sprintf("%4d / %4d data sets\n", meta.i, nrow(meta.not.tt)))
   meta.row <- meta.not.tt[meta.i]
