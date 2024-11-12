ee0a027907115dfeb91d1c2debee9c5fe27885fa
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2024-11-12 16:01:33 -0500

    hjust oclumn

--- data_Classif_batchmark_registry.R
+++ data_Classif_batchmark_registry.R
@@ -885,22 +885,31 @@ for(meta.i in 1:nrow(meta.not.tt)){
       algorithm + data.name + train_subsets + test_subset ~ .,
       list(mean, sd),
       value.var="percent.error")
-    join.wide <- group.meta[scores.wide, on=.(data.name, test_subset)]
+    (join.wide <- group.meta[
+      scores.wide, on=.(data.name, test_subset)
+    ][
+    , hjust := ifelse(percent.error_mean==min(percent.error_mean), 1.1, -0.1)
+    , by=.(test_subset, train_subsets)
+    ][])
     gg <- ggplot()+
       theme_bw()+
       theme(
         ##legend.position="bottom",
         axis.text.x=element_text(angle=30, hjust=1))+
-      ggtitle(paste("Data set:", meta.row$data.name))+
+      meta.row[, ggtitle(sprintf(
+        "Data set: %s (%s)",
+        Data,
+        sub("_","+",data.name)
+      ))]+
       geom_point(aes(
         percent.error_mean, train_subsets, color=algorithm),
         shape=1,
         data=join.wide)+
       geom_text(aes(
         percent.error_mean, train_subsets,
-        hjust=ifelse(algorithm=="featureless", 1.1, -0.1),
+        hjust=hjust,
         label=sprintf(
-          "%.1f±%.f", percent.error_mean, percent.error_sd),
+          "%.1f±%.1f", percent.error_mean, percent.error_sd),
         color=algorithm),
         data=join.wide)+
       scale_color_manual(values=c(
@@ -914,7 +923,7 @@ for(meta.i in 1:nrow(meta.not.tt)){
       facet_grid(. ~ test_subset + subset_rows, labeller=label_both, scales="free")+
       scale_x_continuous(
         "Percent error on CV test subset (mean±SD over 10 folds in CV)",
-        limits=c(0,100),
+        limits=c(-20,120),
         breaks=seq(0,100,by=20))+
       scale_y_discrete(
         "Train subsets")
