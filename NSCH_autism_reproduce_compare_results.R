244d8101ffb6f47ea95b95ebf64a4f290046fa08
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-02-25 14:35:00 -0500

    all

--- NSCH_autism_reproduce_compare_results.R
+++ NSCH_autism_reproduce_compare_results.R
@@ -5,62 +5,66 @@ seq_dt <- fread("NSCH_autism_reproduce_proj_results_sequential.csv")
 reg_dt <- fread("NSCH_autism_reproduce_registry_scores.csv")
 
 compare_dt <- rbind(
-  proj_dt[, .(fun="compute_all_mpi", learner_id, train.subsets, test.subset, test.fold, n.train.groups, classif.auc)],
-  seq_dt[, .(fun="compute_all", learner_id, train.subsets, test.subset, test.fold, n.train.groups, classif.auc)],
-  reg_dt[, .(fun="reduceResultsBatchmark", learner_id, train.subsets, test.subset, test.fold, n.train.groups, classif.auc)]
-)[learner_id=="classif.cv_glmnet"]
+  proj_dt[, .(computation="mpi", learner_id, train.subsets, test.subset, test.fold, n.train.groups, groups, classif.auc, classif.acc)],
+  seq_dt[, .(computation="local", learner_id, train.subsets, test.subset, test.fold, n.train.groups, groups, classif.auc, classif.acc)],
+  reg_dt[, .(computation="batchtools", learner_id, train.subsets, test.subset, test.fold, n.train.groups, groups, classif.auc, classif.acc)]
+)[learner_id=="classif.cv_glmnet" & groups==n.train.groups]
+ucomp <- unique(compare_dt$computation)
 yfac <- function(x)factor(x, c(
-  unique(compare_dt$fun),
   ##"",
-  NULL))
+  unique(compare_dt$computation)))
 compare_dt[, let(
+  train=train.subsets,
+  test=test.subset,
   AUC = classif.auc,
-  Fun = yfac(fun),
-  N_train=n.train.groups
+  Computation = yfac(computation)
 )][]
 compare_stats <- dcast(
   compare_dt,
-  Fun+train.subsets+test.subset+N_train~.,
+  Computation+train+test~.,
   list(mean,sd,length),
   value.var="AUC")
 
-compare_wide <- dcast(compare_dt, train.subsets+N_train+test.subset+test.fold~fun, value.var="AUC")
-compare_long <- nc::capture_melt_single(
+compare_wide <- dcast(
+  compare_dt,
+  train+test+test.fold~Computation,
+  value.var=c("AUC","classif.acc"))
+compare_long <- melt(
   compare_wide,
-  nc::field("compute", "_", ".+"),
-  value.name = "compute_AUC"
-)[
-, ref_AUC := reduceResultsBatchmark
-][]
+  measure.vars=paste0("AUC_", ucomp[-1]),
+  value.name = "AUC_compare",
+  variable.name="compare")
 test_dt <- compare_long[, {
-  tlist <- t.test(ref_AUC, compute_AUC, paired=TRUE)
+  AUC_ref <- .SD[[paste0("AUC_", ucomp[1])]]
+  tlist <- t.test(AUC_compare, AUC_ref, paired=TRUE)
   with(tlist, data.table(p.value, N=.N))
-}, by=.(train.subsets, N_train, test.subset, compute)]
+}, by=.(train, test, compare)]
+
 
 gg <- ggplot()+
-  test_dt[, ggtitle(sprintf("No significant differences\nbetween results computed using different random seeds\nP-value range: %.2f–%.2f", min(p.value), max(p.value)))]+
+  test_dt[, ggtitle(sprintf("No significant differences between Computation methods\n(different random train/test splits, different random subtrain/validation splits)\nP-value range: %.2f–%.2f", min(p.value), max(p.value)))]+
   geom_point(aes(
-    AUC_mean, Fun),
+    AUC_mean, Computation),
     data=compare_stats)+
   geom_text(aes(
-    AUC_mean, Fun, label=sprintf(
+    AUC_mean, Computation, label=sprintf(
       "%.4f±%.4f", AUC_mean, AUC_sd)),
     vjust=-0.5,
     size=3,
     data=compare_stats)+
   geom_segment(aes(
-    AUC_mean+AUC_sd, Fun,
-    xend=AUC_mean-AUC_sd, yend=Fun),
+    AUC_mean+AUC_sd, Computation,
+    xend=AUC_mean-AUC_sd, yend=Computation),
     data=compare_stats)+
   scale_y_discrete(
     "Train subsets",
     drop=FALSE)+
   scale_x_continuous(
     "cv_glmnet performance on test subset (mean±SD over 10 folds in CV)")+
-  facet_grid(train.subsets+N_train~test.subset, scales="free", labeller=label_both)
+  facet_grid(train~test, scales="free", labeller=label_both)
 png(
   "NSCH_autism_reproduce_compare_results.png",
-  width=8, height=9, units="in", res=200)
+  width=8, height=4, units="in", res=200)
 print(gg)
 dev.off()
 
