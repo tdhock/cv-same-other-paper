49b91c0a151ec047d84b924cf934353d97b33179
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-04-16 10:11:36 -0400

    f8fr

---
+++ figure8fr.R
@@ -0,0 +1,23 @@
+library(data.table)
+library(ggplot2)
+score_dt <- fread("conv_images_10fold_test.csv")
+
+plist <- mlr3resampling::pvalue(score_dt[, n.train.groups :=1], value.var="percent_error")
+ent.map <- c(
+  same="même",
+  other="autre",
+  all="tout")
+for(name in c("stats","pvalues")){
+  plist[[name]][, let(
+    Algorithme = factor(algorithm, disp.levs),
+    entraînement = ent.map[paste(Train_subsets)],
+    "sous-ensemble test"=test.subset
+  )]
+}
+gg <- plot(plist)+
+  facet_grid(Algorithme~`sous-ensemble test`, labeller=label_both)+
+  scale_y_discrete("Entraînement",drop=F)+
+  scale_x_continuous("Pourcentage d’erreur de prédiction (moyenne ± écart type, 10 divisions de validation croisée)")
+png("figure8fr.png", width=7, height=8.6, units="in", res=200)
+print(gg)
+dev.off()
